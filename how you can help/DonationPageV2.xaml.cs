5ff0521a111e6258d3777d528020c1fa9aaf2dce
hiang <hiang.guov@gmail.com>
2013-10-10 10:57:01 +1300

    Latest how you can help page, need to fix the layout for the donation page

---
+++ how you can help/DonationPageV2.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Surface;
+using Microsoft.Surface.Presentation;
+using Microsoft.Surface.Presentation.Controls;
+using Microsoft.Surface.Presentation.Input;
+using System.Collections.ObjectModel;
+
+namespace se306Project1
+{
+    /// <summary>
+    /// Interaction logic for DonationPageV2.xaml
+    /// </summary>
+    public partial class DonationPageV2 : Window
+    {
+        public DonationPageV2()
+        {
+            InitializeComponent(); 
+
+        }
+
+        private void AddWindowAvailabilityHandlers()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void back_home(object sender, RoutedEventArgs e)
+        {
+            //HowYouCanHelp is the page name, it will be disapear when user clicks on Home button 
+            HowYouCanHelp.Visibility = System.Windows.Visibility.Collapsed; 
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void submit_email(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void go_HowYouCanHelp_Donate_7(object sender, RoutedEventArgs e)
+        {
+            //HowYouCanHelpTitle.Visibility = System.Windows.Visibility.Collapsed; 
+            donateQrGrid.Visibility = System.Windows.Visibility.Visible;
+            donateEGrid.Visibility = System.Windows.Visibility.Visible;
+        }
+
+        private void go_HowYouCanHelp_Volunteer_8(object sender, RoutedEventArgs e)
+        {
+           //HowYouCanHelpTitle.Visibility = System.Windows.Visibility.Collapsed;
+            donateQrGrid.Visibility = System.Windows.Visibility.Collapsed; 
+            donateEGrid.Visibility = System.Windows.Visibility.Collapsed;
+
+        }
+
+        private void go_HowYouCanHelp_Fundraise_9(object sender, RoutedEventArgs e)
+        {
+            
+        }
+        private void go_HowYouCanHelp_Purchase_10(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Image_ImageFailed(object sender, ExceptionRoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+
+
+    }
+}
